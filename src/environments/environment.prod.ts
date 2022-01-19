8be4f795c5bcd889dcc2c2a07159a791dd95a66d
Matias Pinto <matias25pinto@gmail.com>
2022-01-19 15:28:54 -0300

    ADD(environment):Se agrego keys de producción de firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAsaSMGSxx7mGeGtrrnbBwzgldcRkOFMdg',
+    authDomain: 'fotosfirebase-1ff08.firebaseapp.com',
+    databaseURL: 'https://fotosfirebase-1ff08.firebaseio.com',
+    projectId: 'fotosfirebase-1ff08',
+    storageBucket: 'fotosfirebase-1ff08.appspot.com',
+    messagingSenderId: '425548368888',
+    appId: '1:425548368888:web:dd4ac25e30c7c81c1fa40d',
+    measurementId: 'G-K78FL365T2',
+  },
 };
